096f17b30e7b780f3be64cbf9d364c7631ceb57a
David-F-98 <u2903149@unimilitar.edu.co>
2024-01-03 21:43:52 -0500

    Validación de Cantidad

---
+++ src/validarCantidad.js
@@ -0,0 +1,15 @@
+const formulario = document.getElementById('formulario');
+
+const validarCantidad = () => {
+    const expresionRegularCantidad = /^\d+(\.\d+)?$/;
+    const inputCantidad = formulario.cantidad;
+    if(expresionRegularCantidad.test(inputCantidad.value)){
+       inputCantidad.classList.remove('formulario__input--error');
+       return true;
+    } else {
+        inputCantidad.classList.add('formulario__input--error');
+        return false;
+    }
+};
+
+export default validarCantidad;
