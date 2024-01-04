b562703639aa0f4a4991cc87329665496af22b22
David-F-98 <u2903149@unimilitar.edu.co>
2024-01-03 22:20:24 -0500

    Marcando Paso Como Completo

--- public/bundle.js
+++ public/bundle.js
@@ -14,13 +14,30 @@ const validarCantidad = () => {
     }
 };
 
+const marcarPaso = (paso)=>{
+    document
+    .querySelector(`.linea-pasos [data-paso="${paso}"] span`)
+    .classList.add('linea-pasos__paso-check--checked');
+};
+
 const formulario = document.getElementById('formulario');
 
 formulario.addEventListener('keyup',(e)=>{
     if(e.target.tagName === 'INPUT'){
         if(e.target.id === 'cantidad'){
             validarCantidad();
-        }
-    }
+        }    }});
+
+const btnFormulario = document.getElementById('formulario__btn');
+
+btnFormulario.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const pasoActual = document.querySelector('.linea-pasos__paso-check--active').closest('.linea-pasos__paso').dataset.paso;
+    
+    if(pasoActual === 'cantidad'){
+        
+        if(validarCantidad()){
+            marcarPaso('cantidad');
+        }    }
 });
 //# sourceMappingURL=bundle.js.map
--- src/formulario.js
+++ src/formulario.js
@@ -1,10 +1,25 @@
 import validarCantidad from "./validarCantidad";
+import marcarPaso from "./marcarPaso";
 const formulario = document.getElementById('formulario');
 
 formulario.addEventListener('keyup',(e)=>{
     if(e.target.tagName === 'INPUT'){
         if(e.target.id === 'cantidad'){
             validarCantidad();
-        }
+        };
+    };
+});
+
+const btnFormulario = document.getElementById('formulario__btn');
+
+btnFormulario.addEventListener('click',(e)=>{
+    e.preventDefault();
+    const pasoActual = document.querySelector('.linea-pasos__paso-check--active').closest('.linea-pasos__paso').dataset.paso;
+    
+    if(pasoActual === 'cantidad'){
+        
+        if(validarCantidad()){
+            marcarPaso('cantidad');
+        };
     }
 });
